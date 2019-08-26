c33a83429ece4dcebe446d1d1cc51d32b22dde6b
Lenovo <melisaokt306@gmail.com>
2019-08-26 13:09:09 +0700

    ada yang baru loh!!

---
+++ app/src/main/java/com/viansmul/mapsbox/yuhuu.java
@@ -0,0 +1,14 @@
+package com.viansmul.mapsbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yuhuu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yuhuu);
+    }
+}
